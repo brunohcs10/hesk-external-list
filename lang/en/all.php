7a01c55ce621e9d594be3c66ffe6412506ee5140
Bruno Souza <brunohcs@yahoo.com.br>
2020-05-27 18:49:06 -0300

    Languages

---
+++ lang/en/all.php
@@ -0,0 +1,30 @@
+<?php
+
+$_lang = [
+    'new-ticket' => 'New Ticket',
+    'open-ticket' => 'Open Ticket',
+
+    'status' => 'Status',
+    'category' => 'Category',
+    'last-interaction' => 'Last interaction',
+    'id' => 'ID',
+    'subject' => 'Subject',
+    'reply-by' => 'Reply by',
+    'message' => 'Message',
+    'action' => 'Action',
+    'display-resolved' => 'Display resolved tickets',
+    
+    //'datatable-translate' => '//cdn.datatables.net/plug-ins/1.10.16/i18n/Portuguese-Brasil.json',
+
+    'type-status' => [
+        'new' => 'New',
+        'waiting-reply' => 'Waiting Reply',
+        'replied' => 'Replied',
+        'in-progress' => 'In Progress',
+        'on-hold' => 'On Hold',
+        'resolved' => 'Resolved',
+        'other' => 'Other',
+
+    ],
+
+];
---
+++ lang/pt_BR/all.php
@@ -0,0 +1,30 @@
+<?php 
+$_lang = [
+    'new-ticket' => 'Novo Chamado',
+    'open-ticket' => 'Abrir Chamado',
+
+    'status' => 'Status',
+    'category' => 'Categoria',
+    'last-interaction' => 'Ultima interação',
+    'id' => 'ID',
+    'subject' => 'Assunto',
+    'reply-by' => 'Respondido por',
+    'message' => 'Mensagem',
+    'action' => 'Ação',
+    'display-resolved' => 'Exibir chamados resolvidos',
+    
+    'datatable-translate' => '//cdn.datatables.net/plug-ins/1.10.16/i18n/Portuguese-Brasil.json',
+
+    'type-status' => [
+        'new' => 'Novo',
+        'waiting-reply' => 'Aguardando Resposta',
+        'replied' => 'Respondido',
+        'in-progress' => 'Em Progresso',
+        'on-hold' => 'Em Espera',
+        'resolved' => 'Resolvido',
+        'other' => 'Outro',
+
+    ],
+
+];
+
